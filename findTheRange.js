c33c54f6e824906463787251771a421455c39966
Keegan Wilson <keegan.wilson@gmail.com>
2022-03-22 14:59:54 -0600

    Initial commit

---
+++ findTheRange.js
@@ -0,0 +1,15 @@
+// Starting array
+let array = [28, 43, -12, 30, 4, 0, -36]
+
+// Write your solution below:
+let min = array[0];
+let max = array[0];
+
+for (i = 0; i < array.length - 1; i++) {
+    if (min > array[i]) {
+        min = array[i];
+    }   else if (max < array[i])
+            max = array[i];
+}
+
+console.log(min, max)
